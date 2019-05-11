5840670fc3fcdd9b2c0a127c9c395cb23ad930dc
Parallels <raikhanovaaiym@gmail.com>
2019-05-11 11:36:43 +0600

    remove2

--- .pen_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/parallels/Desktop/project/pen_driver.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/parallels/Desktop/project/pen_driver.ko /home/parallels/Desktop/project/pen_driver.o /home/parallels/Desktop/project/pen_driver.mod.o ;  true
